0533cfaf119bc013f1ab5e883a677ad397a43285
lukalxn <lukalxn@gmail.com>
2023-04-27 23:29:05 -0400

    'Attempting to add ethics model to the langchain script that runs the AI adventure. AS of now, it's all fucky, so I wanted to branch this out to work on seperately from the main script.

--- memory-langchain-test.py
+++ memory-langchain-test.py
@@ -3,7 +3,15 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains.constitutional_ai.base import ConstitutionalChain
+from langchain.chains.constitutional_ai.models import ConstitutionalPrinciple
 import pyfiglet
+
+ethics = ConstitutionalPrinciple(
+    name="Ethical Principle",
+    critique_request="The model should keep the roleplaying game interesting and exciting for players by introducing enemies for players to fight and non-player characters for players to talk and interact with.",
+    revision_request="Rewrite the model's output to include non player characters and attacking enemies when actions get repetitive and predictable.",
+)
 
 llm1 = OpenAI(temperature=0.9)
 
@@ -31,12 +39,19 @@ convo = ConversationChain(
     memory=ConversationBufferMemory()
 )
 
-print(convo.run(proompty.format(firstinput=input())))
+constitutional_chain = ConstitutionalChain.from_llm(
+    chain=convo,
+    constitutional_principles=[ethics],
+    llm=llm1,
+    verbose=True,
+)
+
+print(constitutional_chain.run(convo.run(proompty.format(firstinput=input()))))
 
 for x in range(num):
     print("What would you like to do?")
-    print(convo.run(input()))
+    print(constitutional_chain.run((convo.run(input()))))
 
-print(convo.run("Sum up how the game ends by describing the consequences of my actions. Mention what the elder spirits that are always watching over me thought of my choices."))
+print(constitutional_chain.run(convo.run("Sum up how the game ends by describing the consequences of my actions. Mention what the elder spirits that are always watching over me thought of my choices.")))
 
-print(convo.run("Write a short limerick that summarizes the events of the game and end it with a moral lesson."))
+print(constitutional_chain.run(convo.run("Write a short limerick that summarizes the events of the game and end it with a moral lesson.")))
