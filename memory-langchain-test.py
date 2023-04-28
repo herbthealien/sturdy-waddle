390287a6ec0034ce9fc681b14b9ae4b966f486df
lukalxn <lukalxn@gmail.com>
2023-04-27 22:50:01 -0400

    Implemented fixes to errors in the AI RPG code for memory-langchain-test, using memory chains instead of just simple prompts. Still need to configure the prompting for optimal gameplay as the ending could use some work. That said, the game is functionally playable and generally works as intended. I say that's worth a commit.

--- memory-langchain-test.py
+++ memory-langchain-test.py
@@ -7,11 +7,6 @@ import pyfiglet
 
 llm1 = OpenAI(temperature=0.9)
 
-convo = ConversationChain(
-    llm=ChatOpenAI(temperature=0),
-    memory=ConversationBufferMemory()
-)
-
 welcome = pyfiglet.figlet_format("AI RPG")
 
 print(welcome)
@@ -20,7 +15,7 @@ print("Play a short roleplaying game with artificial intellince as your Dungeon 
 
 print("Please specify the number of turns you'd like to take:")
 
-num = input()
+num = int(input())
 
 print("You are in a dark room that smells of frankincense. What would you like to do?")
 
@@ -29,4 +24,19 @@ proompty = PromptTemplate(
     template="We are playing a roleplaying game together. You are leading the game, you decide what happens when I take actions in the game. The game has started, and I'm in a dark room that smells of frankincense. I would like to {firstinput}. What happens when I do this?"
 )
 
-print(llm1(proompty.format(firstinput=input())))
+#commenting this old code out in case I need it later print(llm1(proompty.format(firstinput=input())))
+
+convo = ConversationChain(
+    llm=OpenAI(temperature=0),
+    memory=ConversationBufferMemory()
+)
+
+print(convo.run(proompty.format(firstinput=input())))
+
+for x in range(num):
+    print("What would you like to do?")
+    print(convo.run(input()))
+
+print(convo.run("Sum up how the game ends by describing what consequences the actions I took had on my immediate surroundings, as well as what affects my choices had on the larger fantasy world that the game is contained within, in a paragraph worthy of a fantasy textbook."))
+
+print(convo.run("Wrap up the game's ending with a thoughtful moral imparted in the style of a short limerick."))
